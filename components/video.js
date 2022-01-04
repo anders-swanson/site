6da8df4b0968e70032bcf11bc28aec75381012dc
anders-swanson <anders.swanson@oracle.com>
2022-01-03 16:10:24 -0800

    Only play videos when in viewport

--- components/video.js
+++ components/video.js
@@ -1,16 +1,28 @@
-export default function Video({ src, controls, height, width }) {
-    const imgWidth = width ? width : "500px"
-    const imgHeight = height ? height : "500px"
-    return (
-        <video
-            width={imgWidth}
-            height={imgHeight}
-            autoPlay
-            muted
-            controls={controls}
-        >
-            <source src={src} type="video/mp4"/>
-        </video>
-    )
+import { useRef } from "react"
+import { Waypoint } from "react-waypoint"
 
+// Only play the video when it is in the viewport.
+// The video will be paused when it is not in the viewport
+export default function Video({ src, height, width, controls}) {
+    const videoRef = useRef(null)
+    const imgHeight = height ? height : "500px"
+    const imgWidth = width ? width : "500px"
+    
+    return (
+        <Waypoint
+            onEnter={() => {videoRef.current.play()}}
+            onLeave={() => {videoRef.current.pause()}}
+        >
+            <video
+                ref={videoRef}
+                width={imgWidth}
+                height={imgHeight}
+                autoPlay
+                muted
+                controls={controls}
+            >
+                <source src={src} type="video/mp4"/>
+            </video>
+        </Waypoint>
+    )
 }

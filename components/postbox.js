ea9df5a59e5fdaa008abada3bf9c3bb0a7800ffa
anders-swanson <anders.swanson@oracle.com>
2022-01-17 16:53:39 -0800

    Add paging

--- components/postbox.js
+++ components/postbox.js
@@ -5,12 +5,18 @@ import Date from '../components/date'
 import Image from 'next/image'
 import { CapitalizeWords } from '../lib/common'
 import { Matches } from '../lib/search'
+import { useState } from 'react'
 
 const imgSizing = 200
+const itemsPerPage = 8
+const selectedStyles = {'fontWeight': 'bold', 'textDecoration': 'underline', 'color': 'black'}
 
 export default function PostBox({ posts, search, heading }) {    
     const headingUpper = CapitalizeWords(heading)
     const filteredPosts = search.length === 0 ? posts : Matches(search, posts)
+    const pages = [...Array(Math.ceil(filteredPosts.length / itemsPerPage)).keys()]
+    const [idx, setIdx] = useState(0)
+    const items = filteredPosts.slice(idx * itemsPerPage, (idx * itemsPerPage) + itemsPerPage)
 
     return (
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -19,7 +25,7 @@ export default function PostBox({ posts, search, heading }) {
         </div>
         <hr/>
         <div className={styles.postBox}>
-          {filteredPosts.map(({ ...item }) => (
+          {items.map(({ ...item }) => (
             <div key={item.id} className={styles.post}>
               <Link href={`/blog${item.id}`}>
                 <a>
@@ -50,10 +56,22 @@ export default function PostBox({ posts, search, heading }) {
               <br/>              
               <small className={utilStyles.lightText}>
                 <Date dateString={item.date} />
-              </small>                                      
+              </small>
             </div>
           ))}
-        </div>        
+        </div>
+        <div className={styles.pageNums}>
+          {pages.map((pageNum) => (
+            <button 
+              style={pageNum === idx ? selectedStyles : {}}
+              onClick={() => setIdx(pageNum)}
+              key={pageNum}
+            >
+              {pageNum+1}
+            </button>
+          ))}
+        </div>      
       </section>
     )
 }
+

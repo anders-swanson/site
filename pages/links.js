55a878fec2770989406c5b9a7b7ac4b205d4214e
anders-swanson <anders.swanson@oracle.com>
2022-04-09 08:40:19 -0700

    Links iframe

--- pages/links.js
+++ pages/links.js
@@ -1,9 +1,20 @@
 import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Post() {
     return (
-        <Layout headerImage='/images/links.JPG'>                
-            <h1>Links</h1>
+        <Layout headerImage='/images/links.JPG'>
+            <div className={utilStyles.centered}>
+                <iframe
+                    src={'https://beacons.ai/laurengastineau/'}
+                    frameBorder='0'
+                    scrolling='no'
+                    width={500}
+                    height='2100px'
+                >
+                    Browser is not compatible.
+                </iframe>
+            </div>
         </Layout>
     )
 }

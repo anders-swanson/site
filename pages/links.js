174d29f2427cd59ddb39d7d38a6b5826974b7f94
anders-swanson <anders.swanson@oracle.com>
2022-04-09 09:54:51 -0700

    remove iframe

--- pages/links.js
+++ pages/links.js
@@ -1,20 +1,9 @@
 import Layout from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
 
 export default function Post() {
     return (
         <Layout headerImage='/images/links.JPG'>
-            <div className={utilStyles.centered}>
-                <iframe
-                    src={'https://beacons.ai/laurengastineau/'}
-                    frameBorder='0'
-                    scrolling='no'
-                    width={500}
-                    height='2100px'
-                >
-                    Browser is not compatible.
-                </iframe>
-            </div>
+            <h1>Links</h1>
         </Layout>
     )
 }

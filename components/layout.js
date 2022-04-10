7e4e1c08a5845e4caacaccfc76029173cab2c7e8
anders-swanson <anders.swanson@oracle.com>
2022-04-10 14:27:35 -0700

    Fix x-origin link

--- components/layout.js
+++ components/layout.js
@@ -59,7 +59,7 @@ export default function Layout({ children, home, allPostsData, postsHeading, hea
       <meta name="description" content={title}/>
       <meta name="keywords" content="hiking, backpacking, oregon"/>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet"/> 
     </Head>
     {isVisible && (

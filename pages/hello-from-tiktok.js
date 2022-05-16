0f19225aef45edc2faceb5b62af454ccf70bf6b0
anders-swanson <anders.swanson@oracle.com>
2022-05-16 16:22:42 -0700

    fix

--- pages/hello-from-tiktok.js
+++ pages/hello-from-tiktok.js
@@ -2,6 +2,6 @@ import Links from "../components/linkpage";
 
 export default function Page() {
     return (
-        <Links title='Hello from TikTok/>
+        <Links title='Hello from TikTok'/>
     )
 }

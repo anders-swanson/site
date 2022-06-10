c89a02dee5b1b001e2f03ae29b08fa0add364dcd
anders-swanson <anders.swanson@oracle.com>
2022-06-10 16:32:04 -0700

    Backpacking Meals

--- scripts/metadata.js
+++ scripts/metadata.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const path = require('path')
-const { stringify } = require('querystring')
 const metadataFlag = '//+metadata'
 
 // Get a sorted list of metadata from all pages in the blogDirectory

f14ab3f83354e879ccda47d06210c6ea4e06fa04
anders-swanson <anders.swanson@oracle.com>
2022-06-22 15:59:52 -0700

    Update spyglass

--- components/search.js
+++ components/search.js
@@ -1,13 +1,21 @@
+import Image from 'next/image'
 import styles from './search.module.css'
 
 export default function Search({search, setSearch}) {
     return (
-        <input
-            className={styles.searchBox}
-            type="text"
-            placeholder=" 🔎︎"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          /> 
+        <>
+            <img
+                alt='spyglass'
+                className={styles.spyglass}
+                src='/search.svg'
+            />
+            <input
+                className={styles.searchBox}
+                type="text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            /> 
+        </>
+
     )
 }

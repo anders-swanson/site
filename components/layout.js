5d4cd9ccee3823449643cecf90f8558b9a419ad0
anders-swanson <anders.swanson@oracle.com>
2022-06-30 16:31:33 -0700

    Pinnable Component

--- components/layout.js
+++ components/layout.js
@@ -63,6 +63,9 @@ export default function Layout({ children, home, allPostsData, postsHeading, hea
         });
     `}
     </Script>
+    <Script id='pinit' strategy='lazyOnLoad'>
+    {`!function(a,b,c){var d,e,f;d="PIN_"+~~((new Date).getTime()/864e5),a[d]?a[d]+=1:(a[d]=1,a.setTimeout(function(){e=b.getElementsByTagName("SCRIPT")[0],f=b.createElement("SCRIPT"),f.type="text/javascript",f.async=!0,f.src=c.mainUrl+"?"+Math.random(),e.parentNode.insertBefore(f,e)},10))}(window,document,{mainUrl:"https://assets.pinterest.com/js/pinit_main.js"});`}
+    </Script>
     <Head>
       <title>{headerText ? headerText : title}</title>
       <link rel="icon" href="logo.jpg" />

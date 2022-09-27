ffe762d951df647659b56107c6cdcd8848499223
anders-swanson <anders.swanson@oracle.com>
2022-09-26 17:01:04 -0700

    Image Grid

---
+++ components/image_grid.js
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './image_grid.module.css'
+
+export default function ImageGrid({ backgroundImage, title, items, opacity, imageWidth, imageHeight }) {
+    opacity = opacity ? opacity : 0.1
+    imageWidth = imageWidth ? imageWidth : 380
+    imageHeight = imageHeight ? imageHeight : 500
+
+    return (
+        <div
+            className={styles.container}
+            style={{
+                'backgroundImage': `url(${backgroundImage}), linear-gradient(rgba(0, 0, 0, ${opacity}), rgba(0, 0, 0, ${opacity}))`,
+            }}
+        >
+            <h1
+                className={styles.page_header}
+            >
+                {title}
+            </h1>
+            <div
+                className={styles.grid_container}
+            >
+                {items.map((item) => (
+                    <div
+                        className={styles.grid_item}
+                    >
+                        <div 
+                            className={styles.grid_image}
+                            style={{
+                                'maxWidth': imageWidth,
+                                'maxHeight': imageHeight,
+                            }}
+                        >
+                            <Link href={item.link}>
+                                <a target="_blank">
+                                    <Image
+                                        src={item.image}
+                                        width={imageWidth}
+                                        height={imageHeight}
+                                    />
+                                </a>
+                            </Link>
+                            
+                        </div>
+
+                        {item.text}
+                    </div>
+                ))} 
+            </div>
+        </div>
+    )
+}
--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet"/> 
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&family=Shrikhand&display=swap" rel="stylesheet"/> 
       </Head>
       <body>
         <Main />

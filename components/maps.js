c537dc3496bab56f2e38befa4be6e38856055c51
anders-swanson <anders.swanson@oracle.com>
2023-01-05 19:15:34 -0800

    Cleanup

--- components/maps.js
+++ components/maps.js
@@ -4,8 +4,12 @@ export default function MapEmbed({ height, width, query, lat, long }) {
     height = height ? height : 400
     width = width ? width : 650
 
-    let url = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}&q=${query}`
-    
+    let q = `${query}`
+    if (lat && long) {
+        q = `${lat},${long}`
+    }
+    const url = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}&q=${q}`
+
     return (
         <div className={utilStyles.centered}>
             <iframe
@@ -16,7 +20,7 @@ export default function MapEmbed({ height, width, query, lat, long }) {
                 }}
                 loading="lazy"
                 allowFullScreen
-                referrerpolicy="no-referrer-when-downgrade"
+                referrerPolicy="no-referrer-when-downgrade"
                 src={url}
             />
         </div>

f6b8e0ed1f0d2ea57a305961a2f50fb30b13cde3
Lauren Gastineau <gastineau.lauren@gmail.com>
2023-01-07 22:22:50 -0800

    adding TabLink comp, gear list, poppins as top global font

---
+++ components/tablink.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function TabLink({href, text}){
+    return (                        
+    <Link href={href}>
+        <a target="_blank">
+            {text}
+        </a>
+    </Link>)
+}

7cabcb68801a6f17545f6a18f83c1e50eaac0388
anders-swanson <anders.swanson@oracle.com>
2023-01-31 16:09:52 -0800

    Custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+export default function FourOhFour() {
+  return (
+    <>
+      <Layout headerText="Page Not Found: 404">
+        <></>
+      </Layout>
+    </>
+  );
+}

cc1f38d10983e29b952b9c1aee838154632e1f87
Anders Swanson <91502735+anders-swanson@users.noreply.github.com>
2023-03-12 15:50:05 -0700

    Dynamic cover image (#82)

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,21 +4,36 @@ import Image from "next/image";
 import utilStyles from "../styles/utils.module.css";
 import React from "react";
 import FeaturedPost from "../components/featuredpost";
+import styles from "../components/index.module.css";
+import IsWindowSize from "../lib/use_window_size";
 
 export default function Home({ posts }) {
+  const windowSize = IsWindowSize(640);
+
   return (
     <Layout
       home
       allPostsData={posts}
       description={`Do you like exploring the outdoors? Join us as we share recent hiking trips, camping suggestions, and backcountry guides.`}
     >
-      <div className={utilStyles.flexGapContainer}>
-        <Image
-          src="/cover.png"
-          alt="Trails and Trekking"
-          width={800}
-          height={400}
-        />
+      <div className={`${utilStyles.flexGapContainer} ${styles.container}`}>
+        {(windowSize && (
+          <Image
+            src="/cover-wide.png"
+            alt="Trails and Trekking"
+            width={1205}
+            height={602}
+          />
+        )) || (
+          <Image
+            src="/cover.png"
+            alt="Trails and Trekking"
+            width={799}
+            height={533}
+          />
+        )}
+
+        <div className={styles.overlayText}>{`Trails & Trekking`}</div>
       </div>
       <br />
       <FeaturedPost post={false} />

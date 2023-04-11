c3c1859f7e427e6886ab4dcd9f2f10670de44fdd
anders-swanson <anders.swanson@oracle.com>
2023-04-11 06:38:24 -0700

    Stubbing hikes

---
+++ components/pages/shorts/coyote-wall.tsx
@@ -0,0 +1,56 @@
+import utilStyles from "../../../styles/utils.module.css";
+import { BestSeason, HikeDifficulty } from "../../../lib/hike_info_types";
+import HikeInfo from "../../hike_info";
+import Image from "next/image";
+import MapEmbed from "../../maps";
+import Slider from "../../slider";
+
+export default function CoyoteWall() {
+  return (
+    <>
+      <h2 className={utilStyles.justifyCenter} id="coyote-wall">
+        Coyote Wall
+      </h2>
+      <HikeInfo
+        distance="7 mile loop"
+        elevation="1,700 feet"
+        difficulty={HikeDifficulty.MODERATE}
+        season={BestSeason.SPRING}
+        trailhead="https://www.google.com/maps/place/Coyote+Wall+Trailhead/@45.7005925,-121.4037556,17z/data=!3m1!4b1!4m6!3m5!1s0x549610435a3707f1:0x6b0035cf329d6726!8m2!3d45.7005888!4d-121.4015669!16s%2Fg%2F11gfc0pt35"
+      />
+      <div>
+        {`Coyote Wall is a popular loop hike for viewing spring wildflowers that's 1 hour 45 minute from Portland by car.
+          This is a fun hike even when its not wildflower season, but during spring it becomes absolutely astounding.
+          On clear, sunny days, you'll get a spectacular view of the Columbia River Gorge and Mt. Hood.`}
+        <br />
+        <br />
+        {`The Coyote Wall loop starts out on a closed road before climbing through a rocky trail section with small waterfalls known as the Labyrinth.
+          After hiking through the Labyrinth, the trail traverses back through flower-filled meadows on its return to the trailhead.`}
+      </div>
+      <div>
+        <Image
+          src="https://lh3.googleusercontent.com/pw/AMWts8AKJxkI7oSllHfiXkwRVa3DXahvLUKRAL_wW0zq2BQItAUC5f6qnYnwTgJqxg-pu-WKru5oduYwk5OdluI5f9XsnnF6jd7C1bVN1GJ1_zBRV5fOGTKbRbIVSDKf9omk-_dpxxHCpDrRDbTp8ViKiZba=w798-h533-s-no?authuser=0"
+          width={798}
+          height={533}
+          alt="Coyote Wall"
+        />
+      </div>
+      <div>
+        {`The latter section of the loop near Coyote Wall (an impressive cliff by the trailhead) is where you'll see the most wildflowers.
+          If you've timed your trip just right, the grassy slopes will be carpeted with colorful flowers of many varieties.`}
+        <br />
+        <br />
+        {`I've linked the trailhead and parking below. Plan to arrive early on weekends, when cars overflow onto the highway by late morning.`}
+      </div>
+      <MapEmbed lat="45.700598" long="-121.401522" />
+      <Slider
+        slides={[
+          "https://lh3.googleusercontent.com/pw/AM-JKLWNWCo6ODzkaM3rxi3J5H3mhRxi235OovfFyaeW-8Odvsrl0OT_nOGtxuei2WV8U4do-KNk7w2dQFp7dfn4hzonlWvZZsWW2xmUhK7-JTFijtdXgVbRN9uMFe9OD1L_NwoJXHpmifdADSMZUiJl6Ajb=w1597-h1067-no?authuser=0",
+          "https://lh3.googleusercontent.com/pw/AM-JKLVOiUpy6nMKjoHglfsjfkPkXBjqe7zFRbipqWgahAMA6Cd-4sHP-TTmEKcSSUWiuX3gnVWbTkDZqeDPV2jx6pQ-gkyDNSy7BznBriFC_tSHMMnlzVEDAv2um-ZChyxLRxxvzpfiJTIv-J3I5G7eOL_b=w1597-h1067-no?authuser=0",
+        ]}
+        width={1200}
+        height={750}
+      />
+    </>
+  );
+}
--- pages/blog/guides/best-hikes-near-portland.tsx
+++ pages/blog/guides/best-hikes-near-portland.tsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 import utilStyles from "../../../styles/utils.module.css";
 import Layout from "../../../components/layout";
 import HikeInfo from "../../../components/hike_info";
-import { BestSeason, HikeDifficulty } from "../../../lib/hike_info_types";
 import Contact from "../../../components/contact";
-import EagleCreek from "../../../components/pages/oregon/eagle-creek-trail";
+import LyleCherryOrchard from "../../../components/pages/shorts/lyle-cherry-orchard";
+import DogMountain from "../../../components/pages/shorts/dog-mountain";
+import CoyoteWall from "../../../components/pages/shorts/coyote-wall";
 
 //+metadata
 const metadata = {
-  title: "Eight Incredible Day Hikes near Portland, Oregon",
-  date: "2023-04-10",
+  title: "My favorite moderate hikes near Portland",
+  date: "2023-04-15",
   image:
     "https://lh3.googleusercontent.com/pw/AMWts8B4dSrNj81huqxNj3QwHsPjwBZcX8sls3dm-ovdW1bGw8jhWumezwGAw_Rt_V9ykRQTrjxtJtI6eYwK4LUarVCH9Z39YOSdBJMqJrN28vmgLu7NRcbJv0EaPNa-lEV_6wP3RuODa1YssKvG3NIJo1kY=w653-h653-s-no?authuser=0",
   tags: ["hiking", "oregon", "guides", "waterfall"],
@@ -19,7 +20,7 @@ export default function Post() {
   return (
     <Layout
       headerText={metadata.title}
-      description={`Discover eight stunning day hikes within driving range of Portland, Oregon.`}
+      description={`Hikes that aren't extremely steep, between 5-8 miles, and are within a 2 hour drive from Portland. Send this to your hiking buddy! 📤`}
       ogImage={metadata.image}
       related={{
         metadata: metadata,
@@ -36,15 +37,31 @@ export default function Post() {
             height={799}
           />
         </div>
-        <EagleCreek readMore={true} />
-        <h2 className={utilStyles.justifyCenter}>Fall Creek Falls</h2>
-        <HikeInfo
-          distance="3 miles round-trip"
-          elevation="900 feet"
-          difficulty={HikeDifficulty.MODERATE}
-          season={BestSeason.YEAR_ROUND}
-          trailhead="https://www.google.com/maps/place/M%C4%81nana+Ridge+Trail/@21.4325135,-157.9392268,15.83z/data=!4m6!3m5!1s0x7c00688823ee72fb:0x40d02d372515187e!8m2!3d21.4301666!4d-157.9387916!16s%2Fg%2F11bzq3ll2w"
-        />
+        <h2 className={utilStyles.centered}>Columbia River Gorge</h2>
+        <h3>{`Angel's Rest`}</h3>
+        <h3>Wahkeena Falls to Multnomah Falls Loop</h3>
+        <DogMountain />
+        <h3>Hamilton Mountain</h3>
+        <CoyoteWall />
+        <LyleCherryOrchard />
+        <h2 className={utilStyles.centered}>Southwest Washington</h2>
+        <h3 className={utilStyles.justifyCenter}>Fall Creek Falls</h3>
+        <h3 className={utilStyles.justifyCenter}>Silver Star</h3>
+        <h2 className={utilStyles.centered}>Mount Hood</h2>
+        <h3 className={utilStyles.justifyCenter}>Ramona Falls</h3>
+        <h3 className={utilStyles.justifyCenter}>Burnt Lake</h3>
+        <h3 className={utilStyles.justifyCenter}>Cloud Cap to Cooper Spur</h3>
+        <h3 className={utilStyles.justifyCenter}>
+          Timberline Lodge to ZigZag Lookout
+        </h3>
+        <h3 className={utilStyles.justifyCenter}>Top Spur to McNeil Point</h3>
+        <h2 className={utilStyles.centered}>Oregon Coast</h2>
+        <h3 className={utilStyles.justifyCenter}>Elk Flats to Cape Falcon</h3>
+        <h3 className={utilStyles.justifyCenter}>{`God's Thumb`}</h3>
+        <h3 className={utilStyles.justifyCenter}>
+          Saddle Mountain (Looks like this hike is still closed 😞)
+        </h3>
+        <h2 className={utilStyles.centered}>Bonus Hike: Silver Falls</h2>
         <Contact />
       </div>
     </Layout>

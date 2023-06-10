5abf4ca5428950ca5caadf47d4dcf5d317adf770
anders-swanson <anders.swanson@oracle.com>
2023-06-10 12:59:16 -0700

    willow creek hs

--- components/maps.tsx
+++ components/maps.tsx
@@ -4,12 +4,16 @@ export default function MapEmbed({
   query = "",
   lat = "",
   long = "",
+  place = "",
 }) {
   let q = `${query}`;
   if (lat && long) {
     q = `${lat},${long}`;
   }
-  const url = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}&q=${q}`;
+  let url = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}&q=${q}`;
+  if (place) {
+    url = `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}&q=place_id:${place}`;
+  }
 
   return (
     <iframe
